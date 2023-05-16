5be40468f4a6c339aa66bed6dbe1b1bdacfa299b
edmond fagbuyi <edmondfagbuyi@gmail.com>
2023-05-16 03:52:11 +0100

    Log Entry into mongodb collection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,59 @@
 package main
 
+import (
+	"context"
+	"log"
+	"time"
 
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
-func main(){
-	
+const (
+	webPort  = "80"
+	rpcPort  = "5001"
+	mongoURL = "mongodb://mongo:27017"
+	gRpcPort = "50001"
+)
+
+var client *mongo.Client
+
+func main() {
+
+	//connect to mongodb
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+
+	//create a context in order to disconnect
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	//close connect
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	//create connect options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	//connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("Error connecting:", err)
+		return nil, err
+	}
+
+	return c, nil
 }
